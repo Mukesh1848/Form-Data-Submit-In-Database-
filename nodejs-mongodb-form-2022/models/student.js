18f6b02fb9ee1741b81c86ddbad09711b36a44bf
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-07-26 08:50:38 +0530

    Update student.js

--- nodejs-mongodb-form-2022/models/student.js
+++ nodejs-mongodb-form-2022/models/student.js
@@ -9,7 +9,7 @@ const studentSchema = new mongoose.Schema({
 })
 
 // Model 
-const studentModel = mongoose.model("student",studentSchema);
+const studentModel = mongoose.model("collectionName",studentSchema);
 
 
 export default studentModel;
