8ca0c128cbc240f9834917e800c617576d3c8a41
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-07-26 08:49:48 +0530

    Update connectdb.js

--- nodejs-mongodb-form-2022/db/connectdb.js
+++ nodejs-mongodb-form-2022/db/connectdb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async(DATABASE_URL)=>{
         try {
               const DB_OPTIONS ={
-                dbName:"mongodbdatabase"
+                dbName:"databasename"
               }  
               await mongoose.connect(DATABASE_URL,DB_OPTIONS);
               console.log("connection successfully");
